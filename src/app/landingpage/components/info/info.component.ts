76dc729cb9e6fcf9c058799d758571399d9ffa96
Jose Cipagauta <jose.cipagauta52@gmail.com>
2024-08-08 20:59:35 -0500

    eliminar resolve configurar service

--- src/app/landingpage/components/info/info.component.ts
+++ src/app/landingpage/components/info/info.component.ts
@@ -1,52 +1,106 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, Renderer2, ViewChild } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { LandingpageService } from '../../services/landingpage.service';
-import { Item } from '../../interfaces/categories';
+import { Category, Item } from '../../interfaces/categories';
 import { map, Observable } from 'rxjs';
+
+import { MatButton } from '@angular/material/button';
 
 @Component({
   selector: 'app-info',
   templateUrl: './info.component.html',
   styleUrl: './info.component.scss'
 })
-export class InfoComponent implements OnInit{
+export class InfoComponent implements AfterViewInit, OnInit {
 
   url: string = '../../../../assets/logoSena.png';
   categoryId: string;
-  items$: Observable<Item[]>;
-  // item$: Observable<Item>;
-  item$: Observable<Item[]>;
-  // test$: Observable<Item>;
-  constructor(private router: Router, private route: ActivatedRoute, public landingpageService: LandingpageService) {
-    this.route.params.subscribe((params)=> this.categoryId = params['id']);
-    this.landingpageService.getItemsByCategory(this.categoryId);
-    this.items$ = this.landingpageService.itemsByCategory$;
+  categoryBy$: Observable<Category>;
+  item$: Observable<Item>;
+  len$: Observable<Number>;
 
-   }
+  disabled: boolean = false;
+
+  // @ViewChild('MyRef', {static: true}) element: ElementRef;
+  // @ViewChild('matButton', { static: true, read: ElementRef }) matButton: ElementRef;
+
+  // @ViewChild("myButton") myButton1: MatButton;
+  // @ViewChild("myButton", { read: ElementRef }) myButtonRef: ElementRef;
+  
+  
+  @ViewChild('myButton', {static: true}) myButtonRef!: ElementRef<HTMLButtonElement>;
+
+  constructor(private router: Router, private route: ActivatedRoute, public landingpageService: LandingpageService, private renderer2:Renderer2) {
+    this.getCategoryById();
+    this.len$ = this.categoryBy$.pipe(map(category => category.items.length));
+  }
 
   ngOnInit(): void {
-    this.item$ = this.items$.pipe(
-      map(items => items.filter(item => { 
-        return item.id == 1
-      }))
+    this.item$ = this.categoryBy$.pipe(
+      map(items => items?.items.find(item => item?.id == 1))
     );
-    // this.item$.subscribe(val => console.log('mitest', val))
-    // this.test$ = this.item$.pipe(map(items => {return items[0]}));
+
+    // console.log(this.myButton1);
+    // console.log(this.myButtonRef.nativeElement)
+    // this.myButtonRef.nativeElement.style.backgroundColor = "orange";
+    // this.myButtonRef.nativeElement.style.border = "solid 1px black";
+
+
     window.scroll(0,0);
+  }
+
+  
+
+  getCategoryById(){
+    this.route.params.subscribe((params)=> this.categoryId = params['id']);
+    this.landingpageService.getCategoryBy(this.categoryId);
+    this.categoryBy$ = this.landingpageService.CategoryBy$;
   }
 
   redirecTo(){
     this.router.navigateByUrl('inicio');
   }
 
-  next(id){
-
+  next(id, len){
     id = Number(id) + 1;
-    this.item$ = this.items$.pipe(
-      map(items => items.filter(item => { 
-        return item.id == id
-      }))
-    );
+
+    
+
+    
+
+    // const myButton = this.element.nativeElement;
+
+    // this.renderer2.setStyle(this.myButtonRef.nativeElement, 'backgroundColor', 'yellow');
+    // this.renderer2.removeClass(this.myButtonRef.nativeElement, 'button-come-back')
+    // this.renderer2.addClass(this.myButtonRef.nativeElement, 'button-come')
+    
+    console.log(id, len);
+    if (id <= len) {      
+      this.item$ = this.categoryBy$.pipe(
+        map(items => items?.items.find(item => item?.id == id))
+      );
+    }
+
+    if (id == len) {
+      this.disabled = true;
+    }
+  }
+
+  ngAfterViewInit() {
+
+    setTimeout(() => {
+      if (this.myButtonRef) {
+        console.log('test',this.myButtonRef.nativeElement);
+        // console.log(this.myButton.nativeElement); // Logs the button element
+      } else {
+        console.error('Button element is not available');
+      }
+    });
+
+
+    // console.log('ngAfter', this.myButtonRef.nativeElement);
+    // this.myButtonRef.nativeElement.textContent = 'esPrueba';
+    // console.log('alert (afterviewinit)', this.element.nativeElement.afterviewinit);
   }
 
 }
--- src/app/landingpage/services/landingpage.service.ts
+++ src/app/landingpage/services/landingpage.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { BehaviorSubject, map, Observable } from 'rxjs';
 import { Category, Item } from '../interfaces/categories';
@@ -11,8 +11,16 @@ export class LandingpageService {
   private categoriesDataSubject = new BehaviorSubject<Category[]>([]);
   public categories$ = this.categoriesDataSubject.asObservable();
 
-  private itemsByCategorySubject = new BehaviorSubject<Item[]>([]);
-  public itemsByCategory$ = this.itemsByCategorySubject.asObservable();
+  private CategoryBySubject = new BehaviorSubject<Category>(null);
+  public CategoryBy$ = this.CategoryBySubject.asObservable();
+
+  private itemsSubject = new BehaviorSubject<Item[]>(null);
+  public items$ = this.itemsSubject.asObservable(); 
+  
+  private itemSubject = new BehaviorSubject<Item>(null);
+  public item$ = this.itemSubject.asObservable(); 
+
+
 
   constructor(private httpClient: HttpClient) { }
 
@@ -20,21 +28,49 @@ export class LandingpageService {
     this.httpClient.get<Category[]>('http://localhost:3000/categories').subscribe(data => {
       this.categoriesDataSubject.next(data);
     });
+
+    // const headers = new HttpHeaders({
+    //   'ngrok-skip-browser-warning': 'true', // Añade la cabecera personalizada
+    //   'User-Agent': 'CustomUserAgent/1.0'   // O usa un User-Agent personalizado
+    // });
+
+    // this.httpClient.get<Category[]>('https://a0eb-191-156-53-129.ngrok-free.app/categories', {headers}).subscribe(data => {
+    //   this.categoriesDataSubject.next(data);
+    // });
   }
 
-  getItemsByCategory(id){
-    this.httpClient.get<Item[]>(`http://localhost:3000/categories/${id}`).subscribe(data => {
-      this.itemsByCategorySubject.next(data);
+  getCategoryBy(id){
+    
+    this.httpClient.get<Category>(`http://localhost:3000/categories/${id}`).subscribe(data => {
+      this.CategoryBySubject.next(data);
     });
+    
+    // const headers = new HttpHeaders({
+    //   'ngrok-skip-browser-warning': 'true', // Añade la cabecera personalizada
+    //   'User-Agent': 'CustomUserAgent/1.0'   // O usa un User-Agent personalizado
+    // });
+
+    // this.httpClient.get<Category>(`https://a0eb-191-156-53-129.ngrok-free.app/categories/${id}`,{headers}).subscribe(data => {
+    //   this.CategoryBySubject.next(data);
+    // });
   }
 
-  // public getCountries(): Observable<Country[]> {
-  //   return this.httpClient.get<CountriesData>('assets/data.json')
-  //   .pipe(
-  //     map (res => {
-  //       console.log('countries', res.countries);        
-  //       return res.countries;
-  //     })
-  //   )
-  // }
+  getItemsByCategory(){
+    this.items$ = this.CategoryBy$.pipe(
+      map(category => {return category.items})
+    );
+  }
+
+  getItemById(index){
+
+    // for (let index = 0; index < this.items$.length; index++) {
+    //   const element = this.items$[index];
+    // }
+
+    // this.items$.forEach(element => {
+    //   if (element.) {
+        
+    //   }
+    // });
+  }
 }
